c345d4b2414b8a1912411156498dfc254e8554af
amps1819 <47330054+amps1819@users.noreply.github.com>
2019-05-13 15:04:32 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# UyA - Práctica 04: Materialize  
+[Página web publicada](https://amps1819.github.io/UyA-Practica04/index.html)
---
+++ js/init.js
@@ -0,0 +1,6 @@
+/* Carrusel
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+*/
